bfeace1a0ae5fc989943a0ae9865ac2e82849797
Ernest <em1027@live.mdx.ac.uk>
2021-11-29 11:18:53 +0000

    enemy movement
    enemy movement works

--- Assets/Scripts/contorls/Enemy.cs
+++ Assets/Scripts/contorls/Enemy.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+    public float Speed;
+    public float distance;
+
+    private bool movingRight = true;
+
+    public Transform groundDetection;
+
+    private void Update()
     {
-        
+
+        transform.Translate(Vector2.right * Speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+        if (groundInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
     }
 }
